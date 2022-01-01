1bc23cdcec31a6565e3b4fbb051610d935d313fb
Ubermensch0608 <melancholyee28@gmail.com>
2022-01-01 19:04:48 +0900

    동적 생성 및 삭제 module 구현

--- Acade_mind/08.A_Complete_Practice_Project/src/App.js
+++ Acade_mind/08.A_Complete_Practice_Project/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import Form from "./Components/Form/Form";
+import UserInfo from "./Components/UserInfo/UserInfo";
 
 function App() {
+  const [newInfo, setNewInfo] = useState([]);
+  const getInfoHandler = (info) => {
+    setNewInfo([
+      {
+        id: info.id,
+        name: info.name,
+        age: info.age,
+      },
+      ...newInfo,
+    ]);
+  };
+
   return (
     <div>
-
+      <Form giveUserInfo={getInfoHandler} />
+      <UserInfo getUserInfo={newInfo} />
     </div>
   );
 }
