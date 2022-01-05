2e3052f9e7c65677190a351034947dc4d78c76c0
Ubermensch0608 <melancholyee28@gmail.com>
2022-01-06 02:38:42 +0900

    recap for section 10

--- Acade_mind/10.Handling_SideEffect,Using_Reducers,Context_API/src/App.js
+++ Acade_mind/10.Handling_SideEffect,Using_Reducers,Context_API/src/App.js
@@ -28,18 +28,13 @@ function App() {
   };
 
   return (
-    <AuthContext.Provider
-      value={{
-        isLoggedIn: isLoggedIn,
-        onLogout: logoutHandler,
-      }}
-    >
-      <MainHeader />
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </AuthContext.Provider>
+    </React.Fragment>
   );
 }
 
