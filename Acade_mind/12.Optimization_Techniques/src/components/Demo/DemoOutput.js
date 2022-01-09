fbab6f079704a308c40844c6d9905138ef5ccd8e
Ubermensch0608 <melancholyee28@gmail.com>
2022-01-10 08:41:39 +0900

    useCallback()과 React.memo()를 사용하여 불필요한 렌더링 최소화

--- Acade_mind/12.Optimization_Techniques/src/components/Demo/DemoOutput.js
+++ Acade_mind/12.Optimization_Techniques/src/components/Demo/DemoOutput.js
@@ -5,4 +5,9 @@ const DemoOutput = (props) => {
   return <p>{props.show ? "This is new!" : ""}</p>;
 };
 
-export default DemoOutput;
+// 큰 component tree를 가지고 있을 때,
+// 부모 component의 렌더링에 의해
+// 자식 component가 불필요한 re-rendering되는 것을
+// 막아줄 수 있는 유용한 툴
+// app의 사이즈에 따라 유용성이 결정됨
+export default React.memo(DemoOutput);
