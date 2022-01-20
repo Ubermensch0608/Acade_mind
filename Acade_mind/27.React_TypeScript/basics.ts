0cc9029ec1ba2583b756857f511600b85a77a658
Ubermensch0608 <melancholyee28@gmail.com>
2022-01-21 01:27:51 +0900

    Add: TypeScript 기초2

--- Acade_mind/27.React_TypeScript/basics.ts
+++ Acade_mind/27.React_TypeScript/basics.ts
@@ -16,4 +16,27 @@ let isInstructor: boolean;
 
 isInstructor = true;
 
-let hobbies: null;
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+// person = {
+//   isEmployee: true,
+// };
+
+let people: {
+  name: string;
+  age: number;
+}[];
