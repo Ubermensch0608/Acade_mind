405dc24d02255eb41ba25114f03df5749d409a7a
Ubermensch0608 <melancholyee28@gmail.com>
2022-01-20 11:34:53 +0900

    Add: react-redux 적용 및 동적인 값 적용

--- Acade_mind/18.Diving_into_Redux/src/components/Counter.js
+++ Acade_mind/18.Diving_into_Redux/src/components/Counter.js
@@ -1,8 +1,24 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const increaseHanlder = () => {
+    dispatch({
+      type: "increase",
+      value: 5,
+    });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -10,6 +26,11 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHanlder}>Increment by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
