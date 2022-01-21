7d3d9290d49b92dddbccb2558adb78a7f2fa07d5
Ubermensch0608 <melancholyee28@gmail.com>
2022-01-22 02:35:12 +0900

    Add: useSelector를 이용해 store의 redux를 선택하여 사용하게 적용

--- Acade_mind/18.prac/src/components/Counter.js
+++ Acade_mind/18.prac/src/components/Counter.js
@@ -1,19 +1,18 @@
-import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import store from "../store";
 
 import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const toggleCounterHandler = (store) => {
-    store.useDispatch({
-      type: "increment",
-    });
-  };
+  const counter = useSelector((state) => state.counter);
+
+  const toggleCounterHandler = (store) => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{store.state}</div>
+      <div className={classes.value}>{counter}</div>
+
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
