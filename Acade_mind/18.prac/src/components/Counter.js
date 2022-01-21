18e7ba5e8e897c78ceacaffb3c9783a6c4e5686f
Ubermensch0608 <melancholyee28@gmail.com>
2022-01-22 02:28:19 +0900

    Add: createStore와 Provider를 이용해 App.js전 범위에 store 사용 가능하게 구현

--- Acade_mind/18.prac/src/components/Counter.js
+++ Acade_mind/18.prac/src/components/Counter.js
@@ -1,12 +1,19 @@
-import classes from './Counter.module.css';
+import { useDispatch } from "react-redux";
+import store from "../store";
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = (store) => {
+    store.useDispatch({
+      type: "increment",
+    });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{store.state}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
