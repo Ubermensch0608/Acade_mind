c5c9c8b1a85d9c91d1f0d26191f5d5a41911d684
Ubermensch0608 <melancholyee28@gmail.com>
2022-02-06 00:02:44 +0900

    Add: 체크박스 체크시 취소선이 생기는 기능 구현

--- Acade_mind/27.React_TypeScript/react-ts-2/src/components/TodoCheck.tsx
+++ Acade_mind/27.React_TypeScript/react-ts-2/src/components/TodoCheck.tsx
@@ -1,5 +1,17 @@
-const TodoCheck = () => {
-  return <input type="checkbox" />;
+import { useRef } from "react";
+
+const TodoCheck: React.FC<{ onChecked: (isCheck: boolean) => void }> = (
+  props
+) => {
+  const checkRef = useRef<HTMLInputElement>(null);
+
+  const checkListner = () => {
+    const isChecked = checkRef.current!.checked;
+
+    props.onChecked(isChecked);
+  };
+
+  return <input type="checkbox" ref={checkRef} onChange={checkListner} />;
 };
 
 export default TodoCheck;
--- Acade_mind/27.React_TypeScript/react-ts-2/src/components/TodoText.tsx
+++ Acade_mind/27.React_TypeScript/react-ts-2/src/components/TodoText.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoCheck from "./TodoCheck";
 
 const TodoText: React.FC<{
   text: string;
   onRemoveTodo: () => void;
 }> = (props) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const isCheckedHandler = (isChecked: boolean) => {
+    setIsChecked(isChecked);
+  };
+
   return (
-    <li onClick={props.onRemoveTodo}>
-      {props.text} <TodoCheck />
+    <li>
+      {isChecked ? <del>{props.text}</del> : <>{props.text}</>}
+      <button onClick={props.onRemoveTodo}>삭제</button>
+      <TodoCheck onChecked={isCheckedHandler} />
     </li>
   );
 };
