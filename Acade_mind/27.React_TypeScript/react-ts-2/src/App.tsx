af81a917f1283480d13449bf4aa769b59da95dc7
Ubermensch0608 <melancholyee28@gmail.com>
2022-02-05 22:06:21 +0900

    Add: TypeScript연습을 위한 초기 세팅 완료

--- Acade_mind/27.React_TypeScript/react-ts-2/src/App.tsx
+++ Acade_mind/27.React_TypeScript/react-ts-2/src/App.tsx
@@ -1,29 +1,5 @@
-import Todos from "./components/Todos";
-import Todo from "./models/todo";
-import NewTodo from "./components/NewTodo";
-import { useState } from "react";
-
 const App = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const saveTodoHandler = (text: string) => {
-    setTodos((prev) => {
-      return [new Todo(text), ...prev];
-    });
-  };
-
-  const removeTodoHandler = (id: string) => {
-    setTodos((prev) => {
-      return prev.filter((todo) => todo.id !== id);
-    });
-  };
-
-  return (
-    <div>
-      <NewTodo onAddTodo={saveTodoHandler} />
-      <Todos items={todos} onRemove={removeTodoHandler} />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
