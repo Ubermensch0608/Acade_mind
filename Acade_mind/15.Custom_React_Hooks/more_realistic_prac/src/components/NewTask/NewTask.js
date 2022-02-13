c7f07e3b7f496905bf4b5fcef897f10732bd2e02
Ubermensch0608 <melancholyee28@gmail.com>
2022-02-13 22:57:28 +0900

    Add: custom hook으로 fetch API 불러오기 완료

--- Acade_mind/15.Custom_React_Hooks/more_realistic_prac/src/components/NewTask/NewTask.js
+++ Acade_mind/15.Custom_React_Hooks/more_realistic_prac/src/components/NewTask/NewTask.js
@@ -1,41 +1,31 @@
-import { useState } from 'react';
+import { useState } from "react";
+import useFetch from "../../hooks/useFetch";
 
-import Section from '../UI/Section';
-import TaskForm from './TaskForm';
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTaskRequests } = useFetch();
+
+  const createdTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    sendTaskRequests(
+      {
+        url: "https://react-http-41e95-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: taskText }),
+      },
+      createdTask.bind(null, taskText)
+    );
   };
 
   return (
